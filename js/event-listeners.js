7e4bbd664807c15af950bf71610ed98cc6bbfc58
AJ-Tan <aldomin22@gmail.com>
2025-07-25 05:30:36 +0800

    initial commit

---
+++ js/event-listeners.js
@@ -0,0 +1,57 @@
+class EventListener {
+   #form = null;
+   #initialElement = null;
+   #toggleElement = null;
+
+   setForm(formClass) {
+      this.#form = document.querySelector(`.${formClass}`);
+
+      if (this.#form) {
+         return this
+      } else {
+         console.error(`Form with class "${formClass}" not found`);
+      }
+   }
+
+   onSubmitForm(initialElement,toggleElement) {
+      this.#form.addEventListener('submit', (e) => {
+         const form = e.currentTarget;
+         this.#initialElement = document.querySelector(`.${initialElement}`);
+         this.#toggleElement = document.querySelector(`.${toggleElement}`);
+
+         e.preventDefault();
+
+         if (form.checkValidity()) {
+            this.toggleVisible();
+         }
+      })
+
+      return this;
+   }
+
+   toggleVisible() {
+         if (!this.#initialElement || !this.#toggleElement) {
+            console.error('Elements not found for toggling visibility');
+            return this;
+         }
+
+         // Toggle visibility
+         this.#initialElement.classList.toggle('is-hidden');  
+         this.#toggleElement.classList.toggle('is-visible');
+   }
+
+   initializeSuccessButton(className) {
+      const successButton = document.querySelector(`.${className}`);
+      
+      if (successButton) {
+         successButton.addEventListener('click', () => {
+            this.toggleVisible();
+         });
+      }
+   }
+}
+
+new EventListener()
+   .setForm('form')
+   .onSubmitForm('newsletter','success')
+   .initializeSuccessButton('success__button');
