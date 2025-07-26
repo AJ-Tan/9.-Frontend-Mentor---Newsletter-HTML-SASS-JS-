8be291c37af53e291d38fdeae8d372265482567f
AJ-Tan <aldomin22@gmail.com>
2025-07-26 12:35:59 +0800

    Add: add form validate logic using validator

--- js/event-listeners.js
+++ js/event-listeners.js
@@ -1,5 +1,6 @@
 class EventListener {
    #form = null;
+   #element = null;
    #initialElement = null;
    #toggleElement = null;
 
@@ -16,17 +17,54 @@ class EventListener {
    onSubmitForm(initialElement,toggleElement) {
       this.#form.addEventListener('submit', (e) => {
          const form = e.currentTarget;
+         const formData = new FormData(form);
+
          this.#initialElement = document.querySelector(`.${initialElement}`);
          this.#toggleElement = document.querySelector(`.${toggleElement}`);
-
+         
          e.preventDefault();
-
-         if (form.checkValidity()) {
+         if (this.isFormDataValid(formData)) {
+            form.classList.remove('is-invalid');
             this.toggleVisible();
+         } else {
+            form.classList.add('is-invalid');
          }
       })
 
       return this;
+   }
+
+   isFormDataValid(formData) {
+      const formEntries = Object.fromEntries(formData);
+
+      //if (!email || !validator.isEmail(email)) return false;
+      for (const key in formEntries) {
+         const inputElement = document.getElementById(key);
+         if(!this.validateData(formEntries[key], inputElement.type)) {
+            inputElement.parentNode.classList.add('is-invalid');
+            return false;
+         } else {
+            inputElement.parentNode.classList.remove('is-invalid');
+         }
+      }
+
+      return true;
+   }
+
+   validateData(data, dataType) {
+      if (dataType === 'email' && validator.isEmail(data)) {
+         return true;
+      }
+
+      if (dataType === 'text' && data.trim() !== '') {
+         return true;
+      }
+
+      if (dataType === 'number' && validator.isNumeric(data)) {
+         return true;
+      }
+
+      return false;
    }
 
    toggleVisible() {
